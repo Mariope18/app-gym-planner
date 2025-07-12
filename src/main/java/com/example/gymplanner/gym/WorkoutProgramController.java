663dc71791d8fe00fbadfd62bbc4333677001c9b
Mario Pella <pellamario18@gmail.com>
2025-07-12 15:48:32 +0200

    Creato controller WorkoutProgramController

---
+++ src/main/java/com/example/gymplanner/gym/WorkoutProgramController.java
@@ -0,0 +1,46 @@
+package com.example.gymplanner.gym;
+
+import com.example.gymplanner.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/programs")
+public class WorkoutProgramController {
+
+    @Autowired
+    WorkoutProgramService programService;
+
+    @PostMapping
+    public ResponseEntity<WorkoutProgram> createProgram(@RequestBody WorkoutProgram workoutProgram,
+                                                        @AuthenticationPrincipal User user){
+        return new ResponseEntity<>(programService.createProgram(workoutProgram,user), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<WorkoutProgram>> findProgramsByUser(@AuthenticationPrincipal User user){
+        return ResponseEntity.ok(programService.findProgramsByUser(user));
+    }
+
+    @GetMapping("/{id}")
+    public  ResponseEntity<WorkoutProgram> findProgramByIdAndUser(@PathVariable Long id, @AuthenticationPrincipal User user){
+        return programService.findProgramByIdAndUser(id,user)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProgram(@PathVariable Long id, @AuthenticationPrincipal User user){
+        boolean isDeleted = programService.deleteProgram(id,user);
+        if (isDeleted){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
